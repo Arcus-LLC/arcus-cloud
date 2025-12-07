f0423f9e16ff8ed55c43663075346fa5145cc018
Bryan Perry <ih57452@gmail.com>
2025-12-07 05:09:21 +0000

    Fix devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
 curl https://mise.run | sh
-echo 'export PATH=\"$HOME/.local/share/mise/shims:$PATH\"' >> ~/.bashrc
+echo "eval \"\$(/home/codespace/.local/bin/mise activate bash)\"" >> ~/.bashrc
 mise trust
+python3 -m pip install --upgrade pip
 pip install pipx
 mise install
-docker logout ghcr.io
+# docker logout ghcr.io
 # helm registry logout ghcr.io
